7f6402ec77ff2eafba29b14e1cdb9054f51fb38c
KarenAmor <karedimor@gmail.com>
2021-10-19 15:37:40 -0500

    Se agregaron formularios, vinculados al login y al registro

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ app.get('/login', (req, res)=> {
     res.sendFile(path.join(__dirname, './views/login.html'));
 });
 
-app.get(('/registro'), (req, res) =>{
-    res.sendFile(path.join(__dirname, '/views/register.html'));
+app.get(('/register'), (req, res) =>{
+    res.sendFile(path.join(__dirname, './views/register.html'));
 
 })
 
 
 app.listen(port, ()=> console.log(`Servidor corriendo ${port}`))
+
