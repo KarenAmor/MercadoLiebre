ce9b1cefc79c719ffc39f79b10601b299d19e989
KarenAmor <karedimor@gmail.com>
2021-10-18 20:44:37 -0500

    Mercado liebre, actividad para trabajar frontend

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3500;
 const publicFolderPath= path.resolve(__dirname, './public')
 app.use(express.static(publicFolderPath));
 app.get('/', (req, res)=> {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+app.get('/login', (req, res)=> {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.get(('/registro'), (req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+
+})
 
 
 app.listen(port, ()=> console.log(`Servidor corriendo ${port}`))
