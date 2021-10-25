0948256dc14cab78c4d594e36750cc1114f96206
KarenAmor <karedimor@gmail.com>
2021-10-24 20:26:30 -0500

    organizando responsive para tablets

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ const path = require('path');
 const port = process.env.PORT || 3500;
 const publicFolderPath= path.resolve(__dirname, './public')
 app.use(express.static(publicFolderPath));
+
 app.get('/', (req, res)=> {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+
+app.get('/', (req, res)=> {
+    res.sendFile(post.resolve(__dirname, './views/home.html'));
+});
+
 app.get('/login', (req, res)=> {
     res.sendFile(path.join(__dirname, './views/login.html'));
 });
